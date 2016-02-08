614121d7820d54508187c2cc0b4343ca9cd3cfe8
Jerry Hu <pengxiang.hu@tuft.edu>
2016-02-08 17:13:19 -0500

    just some random data to see if commit is successful

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var gues: Int = 123
+
+print(gues)
+
+print("🐶🐱🐷🐑")
+
+var names = [14,"Jack", "Jay","John","Jackie"]
+
+for n in names{
+    print(n)
+}
